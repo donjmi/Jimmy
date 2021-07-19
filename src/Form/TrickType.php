f763981348bb3c7b8951af02ad3e899ce7d3cbfb
donjmi <jldonne@gmail.com>
2021-07-19 22:22:09 +0200

    CRUD : Trick

---
+++ src/Form/TrickType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trick;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('content')
+            ->add('publishedAt')
+            ->add('updatedAt')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+}
