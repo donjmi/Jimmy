d74ea312df4109cdf670d69c3ad86b0341411985
donjmi <jldonne@gmail.com>
2021-07-20 10:24:18 +0200

    Update  Forms Trick/Comment

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -3,21 +3,31 @@
 namespace App\Form;
 
 use App\Entity\Trick;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class TrickType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('content')
-            ->add('publishedAt')
-            ->add('updatedAt')
-            ->add('category')
-        ;
+            ->add('name', TextType::class, [
+                "label" => "Nom :"
+            ])
+            ->add('content', TextareaType::class, [
+                "label" => "votre description :"
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                "choice_label" => function ($category) {
+                    return ($category->getName());
+                }
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
