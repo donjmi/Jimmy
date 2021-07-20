290c546308e6e9185731e3e51e299f07c78070ce
donjmi <jldonne@gmail.com>
2021-07-20 20:28:53 +0200

    add pictureController

---
+++ src/Controller/PictureController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Picture;
+use App\Form\PictureType;
+use App\Repository\PictureRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/picture")
+ */
+class PictureController extends AbstractController
+{
+    /**
+     * @Route("/", name="picture_index", methods={"GET"})
+     */
+    public function index(PictureRepository $pictureRepository): Response
+    {
+        return $this->render('picture/index.html.twig', [
+            'pictures' => $pictureRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="picture_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $picture = new Picture();
+        $form = $this->createForm(PictureType::class, $picture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($picture);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('picture_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('picture/new.html.twig', [
+            'picture' => $picture,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="picture_show", methods={"GET"})
+     */
+    public function show(Picture $picture): Response
+    {
+        return $this->render('picture/show.html.twig', [
+            'picture' => $picture,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="picture_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Picture $picture): Response
+    {
+        $form = $this->createForm(PictureType::class, $picture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('picture_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('picture/edit.html.twig', [
+            'picture' => $picture,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="picture_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Picture $picture): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$picture->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($picture);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('picture_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
