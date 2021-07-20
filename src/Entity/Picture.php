6b7a489cbbb5c4e800d6e35f69567db66285259c
donjmi <jldonne@gmail.com>
2021-07-20 20:18:15 +0200

    Add : Picture Entity + Repo

---
+++ src/Entity/Picture.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PictureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PictureRepository::class)
+ */
+class Picture
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Trick::class, inversedBy="pictures")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $tricks;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTricks(): ?Trick
+    {
+        return $this->tricks;
+    }
+
+    public function setTricks(?Trick $tricks): self
+    {
+        $this->tricks = $tricks;
+
+        return $this;
+    }
+}
