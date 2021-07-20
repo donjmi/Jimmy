3d2a58b6d3406d4906dc501832e407036e28e2d7
donjmi <jldonne@gmail.com>
2021-07-20 10:23:18 +0200

    Update trickController functions

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\Trick;
+use App\Entity\Comment;
 use App\Form\TrickType;
+use App\Form\CommentType;
 use App\Repository\TrickRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/trick")
@@ -26,6 +28,18 @@ class TrickController extends AbstractController
     }
 
     /**
+     * @Route("/list", name="trick_list", methods={"GET"})
+     */
+    public function TrickList(TrickRepository $trickRepository): Response
+    {
+        return $this->render('trick/_Trick.html.twig', [
+            'tricks' => $trickRepository->findAll(),
+            'trick_first' => $trickRepository->findBy([], ['id' => 'DESC'], 10, 0),
+            'trick_last' => $trickRepository->findBy([], ['id' => 'DESC'], 50, 10),
+        ]);
+    }
+
+    /**
      * @Route("/new", name="trick_new", methods={"GET","POST"})
      */
     public function new(Request $request): Response
@@ -39,7 +53,8 @@ class TrickController extends AbstractController
             $entityManager->persist($trick);
             $entityManager->flush();
 
-            return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('sucess', 'Trick successfully created');
+            return $this->redirectToRoute('trick_show', ["id" => $trick->getId()]);
         }
 
         return $this->renderForm('trick/new.html.twig', [
@@ -49,17 +64,30 @@ class TrickController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="trick_show", methods={"GET"})
+     * @Route("/show/{id}", name="trick_show")
      */
-    public function show(Trick $trick): Response
+    public function show(Trick $trick, Request $request): Response
     {
+        $comment = new Comment();
+        $comment->setTrick($trick);
+        $form = $this->createForm(CommentType::class, $comment)->handleRequest($request);
+        // dd($form);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $comment->setUser($this->getUser());
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('trick_show', ["id" => $trick->getId()]);
+        }
         return $this->render('trick/show.html.twig', [
             'trick' => $trick,
+            'formComment' => $form->createView()
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="trick_edit", methods={"GET","POST"})
+     * @Route("/edit/{id}", name="trick_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, Trick $trick): Response
     {
@@ -69,7 +97,8 @@ class TrickController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
+            $this->addFlash('sucess', 'Trick successfully updated !');
+            return $this->redirectToRoute('trick_index', ["id" => $trick->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('trick/edit.html.twig', [
@@ -83,10 +112,11 @@ class TrickController extends AbstractController
      */
     public function delete(Request $request, Trick $trick): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$trick->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $trick->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($trick);
             $entityManager->flush();
+            $this->addFlash('info', 'Trick successfully deleted');
         }
 
         return $this->redirectToRoute('trick_index', [], Response::HTTP_SEE_OTHER);
