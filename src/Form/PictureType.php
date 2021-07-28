c3d1faccdea6c6f935097a99402b0cfee6ac5457
donjmi <jldonne@gmail.com>
2021-07-28 12:24:20 +0200

    update: pictureType

--- src/Form/PictureType.php
+++ src/Form/PictureType.php
@@ -6,15 +6,18 @@ use App\Entity\Picture;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PictureType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('tricks')
-        ;
+            ->add('name', FileType::class, [
+                'mapped' => false,
+                'required' => true
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
